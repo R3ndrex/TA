4f365e962ca5311ba4bd4dde630d5da63bff717c
R3ndrex <yacholohchtoli@gmail.com>
2025-05-07 16:09:33 +0300

    add validation for email type

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ questions.forEach((question) => {
         question.classList.toggle("open");
     });
 });
+const emailInput = document.querySelector("input[type='email']");
+emailInput.addEventListener("input", () => {
+    if (emailInput.validity.typeMismatch) {
+        document.querySelector("input[type='email']+span").textContent =
+            "Incorrect email type";
+        document.querySelector("input[type='email']+span").style.display =
+            "block";
+    } else if (emailInput.validity.valueMissing) {
+        document.querySelector("input[type='email']+span").textContent =
+            "This field is required";
+        document.querySelector("input[type='email']+span").style.display =
+            "block";
+    } else {
+        emailInput.setCustomValidity("");
+        document.querySelector("input[type='email']+span").style.display =
+            "none";
+    }
+});
+const submitButton = document.querySelector("[type='submit']");
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+});
